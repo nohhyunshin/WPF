9fdd3dc91caea943a86209c5a487faaac0a4b61c
노현신 <gustls0709@gmail.com>
2025-08-12 20:38:20 +0900

    0812 수정

--- 0811_ex/MainWindow.xaml.cs
+++ 0811_ex/MainWindow.xaml.cs
@@ -24,29 +24,20 @@ namespace _0811_ex
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //string id = "abcd";
-            //string pswd = "1234";
+            string id = "admin";
+            string pswd = "1234";
 
-            string id = txtID.Text;
-            string pswd = txtPSWD.Password;
-
-            if (id == "" || pswd == "")     // IsNullOrWhiteSpace(변수명)
+            if (txtID.Text == "" || txtPSWD.Password == "")
             {
                 MessageBox.Show("아이디와 비밀번호 모두 입력해 주세요.", "경고", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-            MessageBox.Show($"로그인 시도 : {id} / {pswd}", "로그인 정보", MessageBoxButton.OK, MessageBoxImage.Information);
-
-
-            if (id == "admin" && pswd == "1234")     // IsNullOrWhiteSpace(변수명)
+            } else if (txtID.Text == id && txtPSWD.Password == pswd)
             {
-                MessageBox.Show("로그인 성공! 환영합니다.", "로그인 성공", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show($"로그인 성공! 환영합니다.\n로그인 시도 : {txtID.Text} / {txtPSWD.Password}", "로그인 성공", MessageBoxButton.OK, MessageBoxImage.Information);
             }
             else
             {
                 MessageBox.Show("아이디 혹은 비밀번호가 일치하지 않습니다.", "로그인 실패", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
-
         }
     }
 }
---
+++ 0812_ex2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _0812_ex2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
